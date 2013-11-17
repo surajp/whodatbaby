8c54d03843783bb8855a129307dbc7aed9c3e4ec
Spencer Carli <spencercarli@gmail.com>
2013-11-16 20:00:08 -0500

    Finished landing

---
+++ app/js/background-color.js
@@ -0,0 +1,4 @@
+//Enter list of bgcolors:
+var bgcolorlist=new Array("#b4eded", "#F0C2EF", "#90DB7B", "#E8A7A8", "#ED9C32");
+
+document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
